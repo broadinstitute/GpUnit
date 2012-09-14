8a2b444b93bd9158c18e721479d19990cf14382d
pcarr <pcarr@broadinstitute.org>
2012-09-14 15:29:04 +0000

    gp-unit: removed accidently hard-coded gpserver/username/password

--- src/main/java/org/genepattern/gpunit/test/BatchModuleTest.java
+++ src/main/java/org/genepattern/gpunit/test/BatchModuleTest.java
@@ -81,9 +81,9 @@ public class BatchModuleTest {
         //   2) for debugging from an IDE, uncomment the following lines and set accordingly
         //System.setProperty("genePatternUrl", "http://genepattern.broadinstitute.org");
         //System.setProperty("genePatternUrl", "http://genepatternbeta.broadinstitute.org");
-        System.setProperty("genePatternUrl", "http://127.0.0.1:8080");
-        System.setProperty("username", "test");
-        System.setProperty("password", "test"); 
+        //System.setProperty("genePatternUrl", "http://127.0.0.1:8080");
+        //System.setProperty("username", "test");
+        //System.setProperty("password", "test"); 
         //System.setProperty("gpunit.deleteDownloadedResultFiles", "false");
     }
     
